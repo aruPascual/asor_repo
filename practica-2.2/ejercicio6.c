ab881cadd8530dbc0c0b2c0295e1f6a0e3b248f5
aruPascual <44815290+aruPascual@users.noreply.github.com>
2020-12-18 11:59:22 +0100

    pr2: ejercicio 6 hecho

--- practica-2.2/ejercicio6.c
+++ practica-2.2/ejercicio6.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+
 int main()
 {
     const char PATH[] = "/home/alvaro/Escritorio/created_file.txt";
+    mode_t mask = umask(027);
 
-    int fd = open(PATH, O_CREAT, 0645);
-
+    int fd = open(PATH, O_CREAT | O_RDONLY, 0777);
     if (fd < 0)
     {
-        perror("Can't open the file");
+        perror("Error while opening the file");
     }
     else
     {
-        printf("File descriptor %i has been opened\n", fd);
+        printf("File opened succesfully using the mask");
     }
 
     return 0;
