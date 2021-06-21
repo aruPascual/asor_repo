0f6a7fae25e3b675c8f4c31f335f3b4769c5ecdc
aruPascual <alvpas01@ucm.es>
2021-06-21 19:08:19 +0200

    pr5: Update ejercicio9.c

--- practica-2.5/ejercicio9.c
+++ practica-2.5/ejercicio9.c
@@ -1,23 +1,20 @@
-#include <errno.h>
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <netdb.h>
 #include <sys/time.h>
 #include <time.h>
-#include <signal.h>
 #include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-#include <fcntl.h>
-#include <sys/stat.h>
 
-volatile int signal = 0;
+volatile int sig = 0;
 
-void handler(int sig)
+void handler(int signal)
 {
     pid_t pid;
     pid = wait(NULL);
@@ -32,19 +29,19 @@ int main(int argc, char **argv)
     struct sockaddr_storage peer_addr;
     socklen_t peer_addr_len;
     ssize_t nread;
-    char buf[257] = "";
+    char buf[256] = "";
 
     if (argc != 3)
     {
-        printf("Usage: %s <host> <port>\n", argv[0]);
+        printf("Usage: %s <host> <port<\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-    hints.ai_protocol = 0;
+    hints.ai_family = AF_UNSPEC;     /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+    hints.ai_flags = AI_PASSIVE;     /* For wildcard IP address */
+    hints.ai_protocol = 0;           /* Any protocol */
     hints.ai_canonname = NULL;
     hints.ai_addr = NULL;
     hints.ai_next = NULL;
@@ -52,35 +49,34 @@ int main(int argc, char **argv)
     s = getaddrinfo(argv[1], argv[2], &hints, &result);
     if (s != 0)
     {
-        printf("ERROR getaddrinfo: %s\n", gai_strerror(s));
+        perror("ERROR getaddrinfo");
         exit(EXIT_FAILURE);
     }
 
     sfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
     if (sfd == -1)
     {
-        printf("ERROR opening socket\n");
+        perror("ERROR socket");
         exit(EXIT_FAILURE);
     }
 
     if (bind(sfd, result->ai_addr, result->ai_addrlen) != 0)
     {
-        perror("ERROR: unable to bind socket");
+        perror("ERROR bind");
         close(sfd);
         exit(EXIT_FAILURE);
     }
 
-    freeaddrinfo(result);
+    freeaddrinfo(result); /* No longer needed */
     peer_addr_len = sizeof(peer_addr);
 
     if (-1 == listen(sfd, 3))
     {
-        perror("ERROR listening");
+        perror("ERROR listen");
         exit(EXIT_FAILURE);
     }
 
     pid_t pid = getpid();
-
     signal(SIGCHLD, handler);
 
     while (pid)
@@ -88,23 +84,22 @@ int main(int argc, char **argv)
         int acceptedSocket = accept(sfd, (struct sockaddr *)&peer_addr, &peer_addr_len);
         if (acceptedSocket == -1)
         {
-            perror("ERROR accepting conection");
+            perror("ERROR accept");
             exit(EXIT_FAILURE);
         }
 
-        char hostname[NI_MAXHOST];
-        char port[NI_MAXSERV];
-        if (getnameinfo((struct sockaddr *)&peer_addr, peer_addr_len, hostname, NI_MAXHOST, port, NI_MAXSERV, 0))
+        char host[NI_MAXHOST], service[NI_MAXSERV];
+        if (getnameinfo((struct sockaddr *)&peer_addr, peer_addr_len, host, NI_MAXHOST, service, NI_MAXSERV, 0))
         {
-            perror("ERROR getting name info");
+            perror("ERROR getnameinfo");
             exit(EXIT_FAILURE);
         }
-        printf("Conexion desde %s:%s\n", hostname, port);
+        printf("Received from %s:%s\n", host, service);
 
         pid = fork();
         if (pid == -1)
         {
-            perror("ERROR forking");
+            perror("ERROR fork");
             exit(EXIT_FAILURE);
         }
         else if (pid == 0)
@@ -114,14 +109,13 @@ int main(int argc, char **argv)
                 nread = recv(acceptedSocket, buf, 256 * sizeof(char), 0);
                 if (nread == -1)
                 {
-                    perror("ERROR: recieving data");
+                    perror("ERROR recv");
                     close(sfd);
                     exit(EXIT_FAILURE);
                 }
                 else if (nread == 0)
                 {
-                    printf("Conexion terminada\n");
-                    buf[0] = 'q';
+                    printf("Connection expired...\n");
                 }
                 else
                 {
@@ -129,7 +123,7 @@ int main(int argc, char **argv)
                     ssize_t nWrite = send(acceptedSocket, buf, (nread + 1) * sizeof(char), 0);
                     if (nWrite == -1)
                     {
-                        perror("ERROR writing to client");
+                        perror("ERROR send");
                         exit(EXIT_FAILURE);
                     }
                 }
@@ -138,16 +132,14 @@ int main(int argc, char **argv)
         }
         else
         {
-            pid = wait(&stop);
+            pid = wait(&sig);
             close(sfd);
             exit(EXIT_FAILURE);
         }
-        
 
         close(acceptedSocket);
     }
 
     close(sfd);
-
     return 0;
 }
